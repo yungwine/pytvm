b6322fc28557e12902b88ddd6f98ad7c6a8cf8ae
yungwine <cyrbatoff@gmail.com>
2024-03-06 16:00:51 +0700

    update version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class BinaryDistribution(setuptools.dist.Distribution):
 
 setuptools.setup(
     name="pytvm",
-    version="0.0.10",
+    version="0.0.11",
     author="Maksim Kurbatov",
     author_email="cyrbatoff@gmail.com",
     description="Python TVM emulator",
